2dbae053d371089f69653b14af20dbb8a5912714
sleeptightwakebright@hotmail.com <sleeptightwakebright@hotmail.com>
2022-10-21 21:55:05 +1100

    in goes the files

---
+++ resources/javascript/javascript.js
@@ -0,0 +1,27 @@
+const wholeDoc = document.body;
+const createCard = document.getElementById('createCard');
+let cardNo = 2;
+
+const flipCard = (id) => {
+    let theCard = document.getElementById(id);
+    theCard.innerHTML = "1";
+}
+
+const createCardFunction = () => {
+    function createIt() {
+        let create = document.createElement('div');
+        create.className = 'card';
+        create.innerHTML = '<img src="resources/images/CardBack.png" alt="card">';
+        cardNo ++;
+        create.id = cardNo;
+        table.appendChild(create);
+    }
+    createIt();
+    createIt();cd 
+}
+
+createCard.addEventListener('click', createCardFunction);
+
+
+
+
