dd566a89c90989885de754528c94744d2c9b7be2
sleeptightwakebright@hotmail.com <sleeptightwakebright@hotmail.com>
2022-10-31 22:27:01 +1100

    trying (unsucessfully) to get added listners to work

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -3,8 +3,15 @@ const createCard = document.getElementById('createCard');
 let cardNo = 2;
 
 const flipCard = (id) => {
+    console.log(id);
     let theCard = document.getElementById(id);
-    theCard.innerHTML = "1";
+    console.log(theCard.innerHTML);
+    if (theCard.innerHTML === '1') {
+        theCard.innerHTML = '<img src="resources/images/CardBack.png" alt="card">';
+    } else {
+        theCard.innerHTML = '1';
+    }
+    console.log(theCard.innerHTML);
 }
 
 const createCardFunction = () => {
@@ -14,16 +21,26 @@ const createCardFunction = () => {
         create.innerHTML = '<img src="resources/images/CardBack.png" alt="card">';
         cardNo ++;
         create.id = cardNo;
-        create.addEventListener('click', flipCard);
         table.appendChild(create);
         
     }
+
+    function addListener() {
+        let card = document.getElementById(cardNo); 
+        card.addEventListener('click', flipCard(cardNo));
+        console.log('I did it!');
+    }
+
     createIt();
+    addListener()
     createIt(); 
+    addListener()
 }
 
 createCard.addEventListener('click', createCardFunction);
 
-
-
-
+const card1 = document.getElementById('1');
+const card2 = document.getElementById('2');
+card1.onclick = flipCard(card1.id);  
+//card1.addEventListener('click', flipCard(card1.id));
+card2.addEventListener('click', flipCard(card2.id));
