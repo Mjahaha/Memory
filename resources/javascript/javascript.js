1a14e2ba68b8a6d6dff1ed3ae5f547cf9c8d81a1
sleeptightwakebright@hotmail.com <sleeptightwakebright@hotmail.com>
2022-11-05 23:24:40 +1100

    almost have pairs worked out

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -3,11 +3,31 @@ const createCard = document.getElementById('createCard');
 let cardNo = 2;
 let cards =[1,2];
 let flippedCards = [];
+let cardImagesArray = ['bird.png','butterfly.png','cat.png','chook.png','crab.png','dog.jpg','frog.jpg','mouse.jpg','penguin.jpg','sheep.jpg','whale.jpg'];
+
+const allocateImages = arr => {
+    let neededImages = [];
+    let test = [];
+    console.log(cardNo);
+    for (i=0; i < cardNo; i++) {
+        let access = Math.floor(i/2);
+        neededImages.push('resources/images/cards/' + cardImagesArray[access]); 
+        test.push('i');
+    }
+    let output = [];
+    let iterations = neededImages.length;
+    for (i=0; i < iterations; i++) {
+        let rand = Math.floor(Math.random() * neededImages.length);
+        output.push(neededImages[rand]);
+        neededImages.splice(rand, 1);
+    }
+    return output;
+}
+
+let cardImages = allocateImages();
 
 
 const flipCard = event => {
-    console.log(event.target.src);
-    
     let thisCardId;
     if (event.target.parentNode.id == 'table') {
         thisCardId = event.target.id;
@@ -17,14 +37,18 @@ const flipCard = event => {
     }
     let thisCard = document.getElementById(thisCardId);
     let thisCardsImage = thisCard.childNodes[0];
-    flippedCards.push(thisCardId)
 
-
-    if (thisCardsImage.src.includes("resources/images/CardBack.png")) {
-        thisCardsImage.src = 'resources/images/cards/sheep.jpg';
+    if (flippedCards.length < 2) {
+        if (thisCardsImage.src.includes("resources/images/CardBack.png")) {
+            thisCardsImage.src = cardImages[thisCardId-1];
+            flippedCards.push(thisCardId);
+        } 
     } 
     else {
-        thisCardsImage.src = 'resources/images/CardBack.png';
+        console.log(document.getElementById(flipCard[0])); //HOW DO I CONVERT THIS TO A STRING?????!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        document.getElementById(flipCard[0]).innerHTML = '<img src="resources/images/CardBack.png" alt="card">';
+        document.getElementById(flipCard[1].toString()).innerHTML = '<img src="resources/images/CardBack.png" alt="card">';
+        flippedCards = [];
     }
     
 }
@@ -37,22 +61,19 @@ function createIt() {
     create.id = cardNo;
     table.appendChild(create);
     cards.push(cardNo);
-    console.log(cards);
     addListener();
+    cardImages = allocateImages();
 }
 
 function addListener() {
     cards.forEach(cardId => {
         document.getElementById(cardId).addEventListener('click', flipCard);
     })
-    console.log('Listeners added. I did it!');
 }
 
 createCard.addEventListener('click', createIt);
 
 const card1 = document.getElementById('1');
 const card2 = document.getElementById('2');
-//card1.onclick = flipCard;  
 card1.addEventListener('click', flipCard);
 card2.addEventListener('click', flipCard);
-console.log(card1);
